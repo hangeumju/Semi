86ac75c39201d81a02c53abc528bb0e0f11a4fb8
hangeumju <gxxmjx1017@naver.com>
2019-12-18 16:02:56 +0900

    호스트 내정보 및 비밀번호 변경 페이지 구현

---
+++ semi/WebContent/info/host_change_pw_result.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<jsp:include page="/template/header.jsp"></jsp:include>
+<div align="center">
+<h3>비밀번호 변경이 완료되었습니다.</h3>
+<h4><a href="<%=request.getContextPath() %>">메인 페이지로 이동</a></h4>
+</div>
+<jsp:include page="/template/footer.jsp"></jsp:include>
---
+++ semi/src/servlet/Host_Change_PW_Servlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Host_Change_PW_Dao;
+
+@WebServlet(urlPatterns = "/info/host_change_pw.do")
+public class Host_Change_PW_Servlet extends HttpServlet{
+@Override
+protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	try {
+		req.setCharacterEncoding("UTF-8");
+		String host_pw = req.getParameter("host_pw");
+		String host_id = req.getParameter("host_id");
+		
+		Host_Change_PW_Dao HCPdao = new Host_Change_PW_Dao();
+		HCPdao.host_change_password(host_id,host_pw);
+		resp.sendRedirect("host_change_pw_result.jsp");
+		
+	}
+	catch(Exception e) {
+		e.printStackTrace();
+		resp.sendError(500);
+	}
+}
+}
