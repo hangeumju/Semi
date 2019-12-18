3e3f2fc873113e6f37b29bf2d6909b677061d1f4
dnepd13 <56706500+dnepd13@users.noreply.github.com>
2019-12-18 12:32:31 +0900

    콘텐츠 생성 디에이오 디티오 했습니다

--- semi/src/beans/Host_Regist_Dao.java
+++ semi/src/beans/Host_Regist_Dao.java
@@ -15,26 +15,26 @@ public class Host_Regist_Dao {
 	}
 
 	//호스트용 회원가입 메소드입니다
-	public void host_regist(Host_Regist_Dto hdto) throws Exception {
+	public void host_regist(Host_Regist_Dto Hdto) throws Exception {
 		Connection con = getConnection();
 		
-		//인서트로 밑에것을 받습니다
+		//인서트로 밑의 것을 받습니다
 		String sql = "insert into host values(host_no_seq.nextval, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, sysdate, sysdate)";
 		
 		PreparedStatement ps = con.prepareStatement(sql);
 		
-		ps.setString(1, hdto.getHost_id());
-		ps.setString(2, hdto.getHost_pw());
-		ps.setString(3, hdto.getHost_name());
-		ps.setString(4, hdto.getHost_phone());
-		ps.setString(5, hdto.getHost_email_id());
-		ps.setString(6, hdto.getHost_email_domain());
-		ps.setString(7, hdto.getHost_interest());
-		ps.setString(8, hdto.getHost_post());
-		ps.setString(9, hdto.getHost_basic_addr());
-		ps.setString(10, hdto.getHost_extra_addr());
-		ps.setString(11, hdto.getHost_bank_name());
-		ps.setString(12, hdto.getHost_bank_account());
+		ps.setString(1, Hdto.getHost_id());
+		ps.setString(2, Hdto.getHost_pw());
+		ps.setString(3, Hdto.getHost_name());
+		ps.setString(4, Hdto.getHost_phone());
+		ps.setString(5, Hdto.getHost_email_id());
+		ps.setString(6, Hdto.getHost_email_domain());
+		ps.setString(7, Hdto.getHost_interest());
+		ps.setString(8, Hdto.getHost_post());
+		ps.setString(9, Hdto.getHost_basic_addr());
+		ps.setString(10, Hdto.getHost_extra_addr());
+		ps.setString(11, Hdto.getHost_bank_name());
+		ps.setString(12, Hdto.getHost_bank_account());
 		
 		ps.execute();
 		con.close();
