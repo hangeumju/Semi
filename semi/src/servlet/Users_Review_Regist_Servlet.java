ca49828457123db59ddd691464390d53ff1a5f5b
dnepd13 <56706500+dnepd13@users.noreply.github.com>
2019-12-26 15:22:47 +0900

    기타 추가

--- semi/src/servlet/Users_Review_Regist_Servlet.java
+++ semi/src/servlet/Users_Review_Regist_Servlet.java
@@ -25,12 +25,19 @@ public class Users_Review_Regist_Servlet extends HttpServlet{
 			URdto.setReview_content(req.getParameter("review_content"));
 			URdto.setReview_date(req.getParameter("review_date"));
 			
+			String login = req.getParameter("review_writer");
+			boolean isLogin = login != null;
 //			[2]dao를 불러와 users_review_regist 사용, 리뷰 작성 처리를 합니다
 			Users_Review_Dao URdao = new Users_Review_Dao();
+			if(!isLogin) {
 			URdao.users_review_regist(URdto);
+			resp.sendRedirect(req.getContextPath()+"/board/host_content_detail_view.jsp?host_content_no="+ URdto.getContent_original_no());
+			} 
 			
 //			[3]해당 상품 페이지로 다시 이동합니다
+			else {
 			resp.sendRedirect(req.getContextPath()+"/board/host_content_detail_view.jsp?host_content_no="+ URdto.getContent_original_no());
+			}
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
